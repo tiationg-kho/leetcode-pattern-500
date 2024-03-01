a141a9e52b07705f34ed1802f6aea05e51ac1798
tiationg-kho <tiationgkho@gmail.com>
2024-03-01 02:23:29 -0800

    update

--- [A]tree/[A]tree-dfs-postorder/543-diameter-of-binary-tree.java
+++ [A]tree/[A]tree-dfs-postorder/543-diameter-of-binary-tree.java
@@ -15,24 +15,24 @@
  */
 class Solution {
 
-  int res;
+    int res;
 
-  public int diameterOfBinaryTree(TreeNode root) {
-      res = 0;
-      dfs(root);
-      return res;
-  }
+    public int diameterOfBinaryTree(TreeNode root) {
+        res = 0;
+        dfs(root);
+        return res;
+    }
 
-  public int dfs(TreeNode node) {
-      if (node == null) {
-          return 0;
-      }
-      int leftL = dfs(node.left);
-      int rightL = dfs(node.right);
-      int nodeAsPeakL = leftL + rightL;
-      res = Math.max(res, nodeAsPeakL);
-      return Math.max(leftL + 1, rightL + 1);
-  }
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftL = dfs(node.left);
+        int rightL = dfs(node.right);
+        int nodeAsPeakL = leftL + rightL;
+        res = Math.max(res, nodeAsPeakL);
+        return Math.max(leftL + 1, rightL + 1);
+    }
 }
 
 // time O(n), due to traverse
