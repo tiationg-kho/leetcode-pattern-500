a596f91022891ff7191f7072ed86d970619cf6a7
tiationg-kho <tiationgkho@gmail.com>
2024-03-01 02:24:38 -0800

    update

--- [B]linked-list/[B]linked-list/876-middle-of-the-linked-list.java
+++ [B]linked-list/[B]linked-list/876-middle-of-the-linked-list.java
@@ -9,15 +9,15 @@
  * }
  */
 class Solution {
-  public ListNode middleNode(ListNode head) {
-      ListNode slow = head;
-      ListNode fast = head;
-      while (fast != null && fast.next != null) {
-          slow = slow.next;
-          fast = fast.next.next;
-      }
-      return slow;
-  }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 }
 
 // time O(n), due to traverse
