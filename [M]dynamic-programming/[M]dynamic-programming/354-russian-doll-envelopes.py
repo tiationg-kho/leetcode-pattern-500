ebf20abc27b3f44c0081acf5b9d248c0d6f4e7df
tiationg-kho <tiationgkho@gmail.com>
2024-05-11 18:48:00 -0700

    update

--- [M]dynamic-programming/[M]dynamic-programming/354-russian-doll-envelopes.py
+++ [M]dynamic-programming/[M]dynamic-programming/354-russian-doll-envelopes.py
@@ -1,13 +1,24 @@
-import bisect
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+
+        def find_first_larger_or_equal(vals, num):
+            left, right, boundary = 0, len(vals) - 1, - 1
+            while left <= right:
+                m = (left + right) // 2
+                if num <= vals[m]:
+                    boundary = m
+                    right = m - 1
+                else:
+                    left = m + 1
+            return boundary
+
         envelopes.sort(key = lambda x: (x[0], - x[1]))
         dp = []
         for w, h in envelopes:
             if not dp or dp[- 1] < h:
                 dp.append(h)
             else:
-                idx = bisect.bisect_left(dp, h)
+                idx = find_first_larger_or_equal(dp, h)
                 dp[idx] = h
 
         return len(dp)
@@ -15,3 +26,6 @@ class Solution:
 # time O(nlogn)
 # space O(n)
 # using dynamic programming and LIS and patience sort and greedy and binary search and sort
+'''
+1. notice the condition of sorting
+'''
